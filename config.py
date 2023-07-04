5b7ddeb82b86b84c3ef1e981e044916775b8a9ff
lux <kanevegor@icloud.com>
2023-07-04 04:04:54 +0300

    main work done

---
+++ config.py
@@ -0,0 +1,8 @@
+bot_settings = {
+    'TOKEN':'5832553512:AAGoq7ff6sDDHjYCLoUQ3NWifWnVMsMeqgo',
+    'LOG_FILE': 'poll-bot.log',
+}
+
+users_data = {
+    'admin_id' : [659953789, 6150707654]
+}
---
+++ googlesheet_table.py
@@ -0,0 +1,88 @@
+import pygsheets
+from pygsheets.client import Client
+from typing import List, Union
+
+class GoogleTable:
+  def __init__(
+      self, credence_service_file: str = "", googlesheet_file_url: str = ""
+    ) -> None:
+      self.credence_service_file: str = credence_service_file
+      self.googlesheet_file_url: str = googlesheet_file_url
+
+  def _get_googlesheet_by_url(
+      self, googlesheet_client: pygsheets.client.Client
+    ) -> pygsheets.Spreadsheet:
+      sheets: pygsheets.Spreadsheet = googlesheet_client.open_by_url(
+          self.googlesheet_file_url
+        )
+      return sheets.sheet1
+
+  def _get_googlesheet_client(self) -> Client:
+    return pygsheets.authorize(
+          service_file=self.credence_service_file
+        )
+
+  def search_user(
+      self,
+      data: Union[str, List[str]],
+      name_col: int = 1,
+      surname_col: int = 2,
+      post_col: int = 3,
+      tg_col: int = 4,
+      phone_num_col: int = 5,
+      email_col: int = 6,
+      birth_date_col: int = 7,
+      column_name = None
+    ) -> List[dict]:
+      googlesheet_client: pygsheets.client.Client = self._get_googlesheet_client()
+      wks: pygsheets.Spreadsheet = self._get_googlesheet_by_url(googlesheet_client)
+
+      if column_name:
+        find_cells = wks.find(data, matchEntireCell=True, cols=(column_name, column_name))
+
+        if not find_cells:
+          return []
+
+        results = []
+        for cell in find_cells:
+          row = cell.row
+          name = wks.get_value((row, name_col))
+          surname = wks.get_value((row, surname_col))
+          post = wks.get_value((row, post_col))
+          birth_date = wks.get_value((row, birth_date_col))
+          tg = wks.get_value((row, tg_col))
+          phone_num = wks.get_value((row, phone_num_col))
+          results.append({
+                'name': name,
+                'surname': surname,
+                'post': post,
+                'tg': tg
+                })
+
+      else:
+        find_cells = wks.find(data, matchEntireCell=True, cols=(name_col, email_col))
+
+        if not find_cells:
+          return []
+
+        results = []
+        for cell in find_cells:
+          row = cell.row
+          name = wks.get_value((row, name_col))
+          surname = wks.get_value((row, surname_col))
+          post = wks.get_value((row, post_col))
+          email = wks.get_value((row, email_col))
+          birth_date = wks.get_value((row, birth_date_col))
+          tg = wks.get_value((row, tg_col))
+          phone_num = wks.get_value((row, phone_num_col))
+          results.append({
+                'name': name,
+                'surname': surname,
+                'post': post,
+                'email': email,
+                'birth_date': birth_date,
+                'tg': tg,
+                'phone_num': phone_num
+              })
+
+      return results
