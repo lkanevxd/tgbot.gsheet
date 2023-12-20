5c02bc566f083e0bc3ca7433dee2d9eb66a685fe
lux <127681109+lkanevxd@users.noreply.github.com>
2023-12-20 22:10:19 +0300

    update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 bot_settings = {
-    'TOKEN':'5832553512:AAGoq7ff6sDDHjYCLoUQ3NWifWnVMsMeqgo',
+    'TOKEN':'xd',
     'LOG_FILE': 'poll-bot.log',
 }
 
 users_data = {
-    'admin_id' : [6150707654, 580039404, 805172148, 795597693, 877354598]
+    'admin_id' : [xd: int]
 }
